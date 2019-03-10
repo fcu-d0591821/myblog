cb64de7a03dacf011ce611e3e3d995b4d4d0aa49
fcu-d0591821 <d0591821@mail.fcu.edu.tw>
2019-03-10 15:38:15 +0800

    Add admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Tag
 
 # Register your models here.
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text','created_date')
+    fields = ('title', 'text','published_date')
+    search_fields = ('title',)
+    inlines = [TagInline]
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
